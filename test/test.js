385e604599e5e6b7d80cc972502dd5cef8864769
Ilya Shatokhin <Ilyas@pushok.com>
2014-07-24 10:36:00 -0400

    Added chain function

--- test/test.js
+++ test/test.js
@@ -16,20 +16,20 @@ describe("safe",function () {
 				}, function () {
 					assert("Should not be executed")
 			})(new Error());
-		})		
+		})
 		it("should return value on success instead of function execute", function () {
 			safe.sure(function (err,v) {
 				assert(err==null)
 				assert.equal(v,"value")
 				}, "value"
 			)(null);
-		})			
+		})
 		it("should not return value if error happens", function () {
 			safe.sure(function (err,v) {
 				assert(err!=null)
 				}, "value"
 			)(new Error());
-		})		
+		})
 	})
 	describe("trap", function () {
 		it("should rise up exceptions to explicetly provided callback", function () {
@@ -45,7 +45,7 @@ describe("safe",function () {
 			})(null,function (err) {
 				assert(err!=null)
 			});
-		})		
+		})
 	})
 	describe("result", function () {
 		it("should rise up exceptions", function () {
@@ -78,7 +78,7 @@ describe("safe",function () {
 				}, function () {
 					assert("Should not be executed")
 			})(new Error());
-		})		
+		})
 		it("should convert return to callback", function () {
 			safe.sure_result(function (err,v) {
 				assert(err==null)
@@ -86,7 +86,7 @@ describe("safe",function () {
 				}, function () {
 					return "value"
 			})(null);
-		})		
+		})
 	})
 	describe("wrap", function () {
 		it("should rise up exceptions", function () {
@@ -102,7 +102,7 @@ describe("safe",function () {
 			},function (err) {
 				assert(err!=null)
 			})(null);
-		})		
+		})
 	})
 	describe("run", function () {
 		it("should rise up exceptions", function () {
@@ -112,7 +112,7 @@ describe("safe",function () {
 				assert(err!=null)
 			});
 		})
-	})	
+	})
 	describe("spread", function () {
 		it("should convert array to variadic arguments", function () {
 			safe.spread(function (a1,a2,a3) {
@@ -136,7 +136,7 @@ describe("safe",function () {
 				}, function () {
 					assert("Should not be executed")
 			})(new Error());
-		})		
+		})
 		it("should convert array to variadic arguments", function () {
 			safe.sure_spread(safe.noop,function (a1,a2,a3) {
 				assert.equal(a1,"apple");
@@ -144,7 +144,7 @@ describe("safe",function () {
 				assert.equal(a3,"nokia");
 			})(null,["apple","samsung","nokia"])
 		})
-	})	
+	})
 	describe("async", function () {
 		var obj = {
 			doStuff:function (a,b,cb) {
@@ -153,12 +153,12 @@ describe("safe",function () {
 			doBad:function (a,b,cb) {
 				throw new Error();
 			}
-		}		
+		}
 		it("should rise up exceptions", function () {
 			safe.async(obj,"doBad")(function (err,v) {
 				assert(err!=null)
 			})
-		})		
+		})
 		it("should bind to object function and rise up callback value", function () {
 			safe.async(obj,"doStuff",2,3)(function (err,v) {
 				assert(err==null);
@@ -179,7 +179,7 @@ describe("safe",function () {
 			safe.yield(function () { done(a==1?null:new Error("Wrong behavior")) })
 			a++
 		})
-	})	
+	})
 	describe("inherits", function () {
 		var parent = function () {
 		}
@@ -187,7 +187,7 @@ describe("safe",function () {
 		}
 		var child = function () {
 		}
-		safe.inherits(child,parent)		
+		safe.inherits(child,parent)
 		child.prototype.child_function = function () {
 		}
 		it("should make magic that gives child instance methods of parents", function () {
@@ -195,5 +195,29 @@ describe("safe",function () {
 			obj.child_function();
 			obj.parent_function();
 		})
-	})		
+	})
+	describe("chain", function () {
+		it("should execute step by step asynchronous functions in chain", function (done) {
+			var a = 0;
+			safe.chain(function (cb) {
+					safe.yield(function () {
+						a++;
+						cb();
+					});
+				})
+				.then(function (cb) {
+					a++;
+					safe.yield(function () {
+						cb(a === 2 ? null : new Error("Wrong behavior"));
+					});
+				})
+				.then(function (cb) {
+					a++;
+					safe.yield(function () {
+						cb(a === 3 ? null : new Error("Wrong behavior"))
+					});
+				})
+				.done(done);
+		})
+	})
 })
