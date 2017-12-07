758894ed2af3deebd7bf06cfeeaafd4a02dc87e4
Ilya Shatokhin <ilyas@pushok.com>
2017-12-07 11:22:27 -0200

    Don't shadow/reuse arguments.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function checkES6 () {
 	"use strict";
 
 	try {
-		new Function ('a', 'class orig { constructor () { } }; class cls extends orig { constructor () { super() } }; var foo = (...args) => [].push(...a); return foo;')([]);
+		new Function('a', 'class orig { constructor () { } }; class cls extends orig { constructor () { super() } }; var noop = () => { }; var foo = (f = noop, ...args) => [].push(...a); return foo;')([]);
 		return './lib/safe.modern.js';
 	} catch (err) {
 		return './lib/safe.js';
